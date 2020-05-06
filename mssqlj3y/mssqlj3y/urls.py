4a1d793b8beb09e090ef07898e4628c2ba58f9f1
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-05-06 17:03:52 +0800

    Update urls.py

--- mssqlj3y/mssqlj3y/urls.py
+++ mssqlj3y/mssqlj3y/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mail_job/', include('mail_job.urls')),
+    path('', RedirectView.as_view(url='mail_job/')),
 ]
+# my urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
