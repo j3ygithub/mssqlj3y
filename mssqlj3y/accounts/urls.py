0f2103a0ea86709fd8f53242d29c4aec7ac37c9a
Jimmy Lin <jimmy_lin@example.com>
2020-05-08 19:13:59 +0800

    new sign up view
    new sign up view

--- mssqlj3y/accounts/urls.py
+++ mssqlj3y/accounts/urls.py
@@ -17,5 +17,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign_up/', views.SignUp.as_view(), name='signup'),
+    path('sign_up/', views.signup, name='signup'),
 ]
--- mssqlj3y/accounts/views.py
+++ mssqlj3y/accounts/views.py
@@ -4,6 +4,24 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/sign_up.html', {'form': form})
+
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
