eefeb479d4daca85734c5b5cd9a01d0ff97dd1e0
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-05-10 08:33:39 +0800

    Update views.py
    remove unnecessary accounts/views.py codes

--- mssqlj3y/accounts/views.py
+++ mssqlj3y/accounts/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
 
 def signup(request):
     if request.method == 'POST':
@@ -20,10 +16,7 @@ def signup(request):
             return redirect('/')
     else:
         form = UserCreationForm()
-    return render(request, 'registration/sign_up.html', {'form': form})
-
-
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/sign_up.html'
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/sign_up.html', context)
