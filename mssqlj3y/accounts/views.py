e57376510c718a50ea99e412062fa9ce583e0f79
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-05-11 17:20:27 +0800

    almost done the cheif email sign up part
    execpt for the send email fail and finished's next step

--- mssqlj3y/accounts/views.py
+++ mssqlj3y/accounts/views.py
@@ -4,25 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
-import uuid
-
-from django import forms
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-
-
-class ChiefUserSignUpForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['email']
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if '@chief.com.tw' not in email:
-            raise ValidationError(f'The Email address must contain "@chief.com.tw".')
-        if User.objects.filter(email=email).exists():
-            raise ValidationError(f'This Email has already been used.')
-        return email
+from .forms import ChiefUserSignUpForm
 
 
 def sign_up_with_chief_email(request): 
--- mssqlj3y/mssqlj3y/settings.py
+++ mssqlj3y/mssqlj3y/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mssqlj3y.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+from secret.mssqlj3y.settings import DATABASES
+DATABASES = DATABASES
 
 
 # Password validation
