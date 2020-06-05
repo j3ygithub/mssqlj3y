9bbc97587f26a587a30fc5b0b6f8355db80d26cb
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-06-05 13:12:00 +0800

    update sign_up_with_chief_email template, form, validation

--- mssqlj3y/accounts/forms.py
+++ mssqlj3y/accounts/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
+from django.utils.translation import gettext_lazy as _
 
 class ChiefUserSignUpForm(forms.ModelForm):
+
+    email = forms.EmailField(
+        label=_('Chief Email'),
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder': _('Chief Email'),
+            },
+        ),
+    )
+
     class Meta:
         model = User
         fields = ['email']
@@ -11,7 +22,7 @@ class ChiefUserSignUpForm(forms.ModelForm):
     def clean_email(self):
         email = self.cleaned_data['email']
         if '@chief.com.tw' not in email:
-            raise ValidationError(f'The Email address must contain "@chief.com.tw".')
+            raise ValidationError(_('The Email address must contain "@chief.com.tw".'))
         if User.objects.filter(email=email).exists():
-            raise ValidationError(f'This Email has already been used.')
+            raise ValidationError(_('This Email has already been used.'))
         return email
