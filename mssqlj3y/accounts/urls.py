e6fbfb41bbe4c6b4ff1fec50ab20618947cf8d60
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-06-06 21:06:25 +0800

    update i18n

--- mssqlj3y/accounts/urls.py
+++ mssqlj3y/accounts/urls.py
@@ -22,11 +22,10 @@ urlpatterns = [
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
-
 # some custom views
 urlpatterns += [
     path('sign-up/', sign_up, name='sign_up'),
     path('sign-up/with-account-password/', sign_up_with_account_password, name='sign_up_with_account_password'), # not shown directly on web interface
     path('sign-up/with-chief-email/', sign_up_with_chief_email, name='sign_up_with_chief_email'),
-    path('profile_chnage', profile_change, name='profile_change'),
+    path('profile/', profile_change, name='profile_change'),
 ]
