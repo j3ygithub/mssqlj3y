4a881f3245c085194b89c9a7792c07d5fee9227b
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-06-14 02:58:17 +0800

    update some js things

--- mssqlj3y/mail_job/static/js/mail_job/change_list.js
+++ mssqlj3y/mail_job/static/js/mail_job/change_list.js
@@ -5,8 +5,6 @@ $(document).ready(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
-});
-$(document).ready(function () {
     $("#field-controller-mail-job").on("change", function () {
         var value = $(this).val();
         if (value == "basic_field") {
@@ -16,8 +14,6 @@ $(document).ready(function () {
             $('#table-mail-job .advanced-field').show();
         }
     });
-});
-$(document).ready(function () {
     $("#show-all-controller-mail-job").on("change", function () {
         var value = $(this).val();
         var role = "{{ user }}"
@@ -29,12 +25,12 @@ $(document).ready(function () {
         else {
             $("#table-mail-job tbody tr").show()
         }
+        var showAllMailJob = $("#show-all-controller-mail-job").val();
+        var role = "{{ user }}"
+        if (showAllMailJob == 'personal') {
+            $("#table-mail-job tbody tr").filter(function () {
+                $(this).toggle($(this).text().indexOf(role) > -1)
+            });
+        }
     });
 });
-var showAllMailJob = $("#show-all-controller-mail-job").val();
-var role = "{{ user }}"
-if (showAllMailJob == 'personal') {
-    $("#table-mail-job tbody tr").filter(function () {
-        $(this).toggle($(this).text().indexOf(role) > -1)
-    });
-}
